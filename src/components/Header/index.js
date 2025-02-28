923eb0903ebac0d2afce87b5e7ec1e700fecd7bc
vidya <vidya.palliyalil@gmail.com>
2025-02-28 17:54:56 +0530

    Remove unused assets and add initial components for Cart, ProductCard, and ProductList

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <nav className='navbar navbar-dark bg-dark'>
+    <span className='navbar-brand m-4'>E-commerce</span>
+    <span className='text-light m-4'>Cart</span>
+   </nav>
+  )
+}
+
+export default Header
