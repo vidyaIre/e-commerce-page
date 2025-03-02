76cbd2b0980f76d73e70e1d188d7c2eeff4f4f5b
vidya <vidya.palliyalil@gmail.com>
2025-03-02 15:55:09 +0530

    Add react-router-dom and implement routing in App component; enhance ProductCard to accept props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
-import ProductCard from './components/ProductCard';
+import { ProductCard } from './components/productCard';
 
 function App() {
   return (
-    <div className="App">
-      <>
-        <Header />
-        <ProductCard />
-      </>
-    </div>
-  );
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element = {<Header/>}/>
+        <Route path="/ProductCard" element = {<ProductCard/>}/>
+      </Routes>
+    </Router>
+    </>
+    );
 }
 
 export default App;
--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const ProductCard = () => {
+const ProductCard = ({ product, addToCart }) => {
   return (
-    <div>ProductCard</div>
-  )
-}
+    <div className="card">
+      <img src={product.image} className="card-img-top" alt={product.name} />
+      <div className="card-body">
+        <h5 className="card-title">{product.name}</h5>
+        <p className="card-text">{product.description}</p>
+        <p className="card-text">${product.price}</p>
+        <button className="btn btn-primary" onClick={() => addToCart(product)}>Add to Cart</button>
+      </div>
+    </div>
+  );
+};
 
 export default ProductCard
